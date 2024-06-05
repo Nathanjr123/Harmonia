4992972277ae495e26285ec0b1d154d1f8a7d1ef
nathan <nathanphysicz@gmail.com>
2024-06-05 09:35:08 +0200

    Added profile feature

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,6 +2,26 @@
 pragma solidity 0.8.20;
 
 contract Counter {
+
+    struct Profile{//profile strutc
+        string name;
+        uint8 age;
+        string gender;
+    }
+    mapping(address => Profile) private profiles;
+    event ProfileSet(address indexed user, string name, uint8 age, string gender);
+    
+    function setProfile(string memory _name, uint8 _age, string memory _gender) public {
+
+        profiles[msg.sender] = Profile(_name,_age,_gender);
+        emit ProfileSet(msg.sender, _name,_age,_gender);
+    }
+    function getProfile(address _user) public view returns (string memory, uint8, string memory) {
+        Profile memory profile = profiles[_user];
+        return (profile.name, profile.age, profile.gender);
+
+    }
+
     uint256 private number;
 
     function setNumber(uint256 newNumber) public {
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.20;
 import "./TestSetup.t.sol";
 
 contract CounterTest is TestSetup {
+
+
     function test_Increment() public {
         assertEq(counter.getNumber(), 0);
         counter.increment();
@@ -19,4 +21,21 @@ contract CounterTest is TestSetup {
         counter.setNumber(x);
         assertEq(counter.getNumber(), x);
     }
+    function test_SetProfile() public {
+    console.log("This is a test");
+
+    string memory name = "Nate";
+    uint8 age = 30;
+    string memory gender = "Male";
+
+    counter.setProfile(name, age, gender);
+
+    (string memory storedName, uint8 storedAge, string memory storedGender) = counter.getProfile(msg.sender);
+    console.log("Stored Name:", storedName);
+
+    assertEq(storedName, name);
+    assertEq(storedAge, age);
+    assertEq(storedGender, gender);
+}
+
 }
