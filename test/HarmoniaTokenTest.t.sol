88385fe547006db7d86a0e88c0da83f4d0790be6
Nathan <nathan@linumlabs.com>
2024-07-01 13:04:51 +0200

    Create basic contract for Har,onia token

--- test/HarmoniaTokenTest.t.sol
+++ test/HarmoniaTokenTest.t.sol
@@ -4,20 +4,19 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "../src/HarmoniaToken.sol";
 
-contract TestSetup is Test {
+contract HarmoniaTokenTest is Test {
     HarmoniaToken public harmoniaToken;
 
     address public owner = address(this);
     address public addr1 = address(0x123);
 
-    function setUp() public virtual {
+    function setUp() public {
         harmoniaToken = new HarmoniaToken();
         harmoniaToken.transferOwnership(owner);
     }
 
-    function testInitialSetup() public view {
+    function testInitialSupply() public view {
         assertEq(harmoniaToken.totalSupply(), 0);
-        assertEq(harmoniaToken.owner(), owner);
     }
 
     function testMint() public {
