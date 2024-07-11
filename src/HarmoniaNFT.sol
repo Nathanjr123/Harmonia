2fb05f2cb08c3d5d7fc83bbd093a45afd4f99703
Nathan <nathan@linumlabs.com>
2024-07-11 09:28:23 +0200

    git fixes

--- src/HarmoniaNFT.sol
+++ src/HarmoniaNFT.sol
@@ -6,26 +6,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract HarmoniaNFT is ERC721, Ownable {
     error NotApprovedOrOwner(address addr, uint256 tokenId);
-    error InvalidTokenId(uint256 tokenId);
 
     constructor() ERC721("HarmoniaNFT", "HNFT") Ownable(msg.sender) {}
 
-    function mint(address to, uint256 tokenId) public onlyOwner {
-        if (to == address(0)) {
-            revert InvalidTokenId(tokenId);
-        }
-        _mint(to, tokenId);
-    }
-
-    function burn(uint256 tokenId) public {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override {
         if (!_isApprovedOrOwner(msg.sender, tokenId)) {
             revert NotApprovedOrOwner(msg.sender, tokenId);
         }
-        _burn(tokenId);
-    }
-
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
-        address owner = ownerOf(tokenId);
-        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
